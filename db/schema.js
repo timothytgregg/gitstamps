0c30aa74feb27379e668f9331338920803258b44
Chase Gruber <chasegruber@gmail.com>
2015-12-04 14:48:45 -0500

    configure profile and gitstamp schemas

---
+++ db/schema.js
@@ -0,0 +1,21 @@
+// requiring mongoose dependency
+var mongoose = require('mongoose')
+
+// instantiate a name space for our Schema constructor defined by mongoose.
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId
+
+// defining schema for reminders
+var GitstampSchema = new Schema({
+  data: Float
+})
+
+// defining schema for authors.
+var ProfileSchema = new Schema({
+  name: String,
+  gitstamps: [GitstampSchema]
+})
+
+// setting models in mongoose utilizing schemas defined above
+var ProfileModel = mongoose.model("Profile", ProfileSchema)
+var GitstampModel = mongoose.model("Gitstamp", GitstampSchema)
