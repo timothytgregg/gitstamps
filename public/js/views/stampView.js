365239e95f4e0d8a08b4246937f7cbc4b517fe3d
Chase Gruber <chasegruber@gmail.com>
2015-12-10 11:24:12 -0500

    functional repo bar chart

--- public/js/views/stampView.js
+++ public/js/views/stampView.js
@@ -53,6 +53,7 @@ StampView.prototype = {
     var data = d3.entries(this.stamp.data.languages)
       .map(function(d){
         var values = d3.entries(d.value).filter(function(a){return a.key!=='meta'})
+          .sort(function(a,b){return a.value - b.value})
         return {
           key: d.key,
           meta: d.value.meta,
@@ -67,13 +68,11 @@ StampView.prototype = {
       })
     repoLangs.forEach(function(repo){
       var thisSum = 0;
-      console.log(repo)
       for (var i=0;i<repo.value.length;i++){
         thisSum+=repo.value[i].value;
       }
       if(thisSum > repoMax){repoMax = thisSum}
     });
-    console.log(repoMax)
     return {repoLangs:repoLangs, repoMax:repoMax};
   },
   makeRepoComposite:function(g,data,repoMax){
@@ -87,6 +86,17 @@ StampView.prototype = {
         return yScale(d.value)
       })
       .attr('width',w)
+      .attr('transform',function(d,i){
+        var vals = d3.select(this.parentNode).datum().value;
+        var offset = 0;
+        for (var b=0;b<i;b++){
+          offset += yScale(vals[b].value);
+        }
+
+
+
+        return 'translate(0,'+(300-offset-yScale(d.value))+')'
+      })
       .style('fill',function(d){return githubColors[d.key]})
   },
   render: function(stampsDiv){
