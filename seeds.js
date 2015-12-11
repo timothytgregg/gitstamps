e0fdfa1f234efe4f7e11ce93589c9166a7265c1e
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-11 10:51:20 -0500

    paths in seeds.js

--- seeds.js
+++ seeds.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose')
 // connects us to the reminders database in mongo
 var conn = mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://localhost/chase-express')
 // require our model definitions we defined earlier
-var ProfileModel = require("../models/profile")
-var StampModel = require("../models/stamp")
-var UserModel = require("../models/user")
+var ProfileModel = require("./models/profile")
+var StampModel = require("./models/stamp")
+var UserModel = require("./models/user")
 // removes any existing profiles and stamps from our database
 ProfileModel.remove({}, function(err){
 })
